d10295afca72ec8d4a947f48bc1291a90bd6fa8a
ruben <469470@student.saxion.nl>
2021-09-23 23:12:22 +0200

    data of auctions

---
+++ src/data/replicas.js
@@ -0,0 +1,135 @@
+// pistolen
+[
+    {
+        "id" : 1,
+        "name" : "AAP-01 ASSASSIN | BLACK | GBB | ACTION ARMY",
+        "type" : "GBB",
+        "system" : "Groen Gas",
+        "materail" : "Polymeer",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" :"",
+    },
+    {
+        "id" : 2,
+        "name" : "DESERT EAGLE DE .50 SILVER METAL VERSION | CO2 | KWC",
+        "type" : "CO2",
+        "system" : "CO2",
+        "materail" : "Full Metal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    },
+    {
+        "id" : 3,
+        "name" : "HI-CAPA 5.1 GOLD MATCH | GBB | TOKYO MARUI",
+        "type" : "GBB",
+        "system" : "Groen Gas",
+        "materail" : "Polymeer / Metaal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    }
+]
+
+// geweren
+[
+    {
+        "id" : 4,
+        "name" : "LT-29 ENFORCER NEEDLETAIL BLACK | AEG | LANCER TACTICAL",
+        "type" : "AEG",
+        "system" : "electric",
+        "materail" : "Polymeer / Metaal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    },
+    {
+        "id" : 5,
+        "name" : "MTR16 G-EDITION ZET-SYSTEM CERAKOTE VERSION | GBBR | TOKYO MARUI",
+        "type" : "GBB",
+        "system" : "Groen Gas",
+        "materail" : "Full Metal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    },
+    {
+        "id" : 6,
+        "name" : "WOLVERINE MTW 14,5 BARREL WITH INFERNO GEN 2",
+        "type" : "HPA",
+        "system" : "CO2",
+        "materail" : "Full Metal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    }
+]
+
+// SMG
+[
+    {
+        "id" : 7,
+        "name" : "ARP9 | AEG | G&G",
+        "type" : "AEG",
+        "system" : "electric",
+        "materail" : "Polymeer / Metaal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    },
+    {
+        "id" : 8,
+        "name" : "H&K UMP .45 DX FULL POWER BLACK | GBR | VFC",
+        "type" : "GBB",
+        "system" : "Groen Gas",
+        "materail" : "Polymeer / Metaal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    },
+    {
+        "id" : 9,
+        "name" : "KWC MINI SUB-MACHINE CO2 UZI",
+        "type" : "CO2",
+        "system" : "CO2",
+        "materail" : "Full Metal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    }
+]
+
+// Sniper
+[
+    {
+        "id" : 10,
+        "name" : "L96 SNIPER RIFLE SET | BLACK | WELL",
+        "type" : "Bolt action",
+        "system" : "veerdruk",
+        "materail" : "Kunststof",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    },
+    {
+        "id" : 11,
+        "name" : "M107 BLACK | AEG | SNOW WOLF",
+        "type" : "AEG",
+        "system" : "electric",
+        "materail" : "Full Metal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    },
+    {
+        "id" : 12,
+        "name" : "PGM 338 | BOLT ACTION | GAS | SNIPER",
+        "type" : "GBB",
+        "system" : "Groen Gas",
+        "materail" : "Full Metal",
+        "price" : 100,
+        "biddings" : 1,
+        "end_date" : "",
+    }
+]
