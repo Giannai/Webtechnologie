40b16bc34d44c98d1a4c5f560cdace4c7ac23670
Giannai <500306@student.saxion.nl>
2021-09-26 21:38:31 +0200

    Auction api calls updated

--- src/routes/auctions.js
+++ src/routes/auctions.js
@@ -6,27 +6,20 @@ const auctions = require('../data/replicas')
 const router = express.Router();
 
 // // Return auction list (auctions)
-// router.get('', (req,res)=> {
-//
-//     // Create empty list for the usernames
-//     const listReplicas = [];
-//
-//     // for each user in users, push all usernames to the list created above
-//     for (const replica of auctions) {
-//         let jsonReplica = `{"name" : "${replica.name}", "link" : "http://localhost:3000/replicas/auction/${replica.id}"}`;
-//         let replicaObj = JSON.parse(jsonReplica);
-//         listReplicas.push(replicaObj);
-//     }
-//
-//     if (listReplicas)
-//         res
-//             .status(StatusCodes.OK)
-//             .send(listReplicas);
-//     else
-//         res
-//             .status(StatusCodes.NOT_FOUND)
-//             .send('');
-// })
+router.get('', (req,res)=> {
+
+    // Create empty list for the usernames
+    const listReplicas = [];
+
+    // for each user in users, push all usernames to the list created above
+    for (const replica of auctions) {
+        let jsonReplica = `{"name" : "${replica.name}", "link" : "http://localhost:3000/replicas/auction/${replica.id}"}`;
+        let replicaObj = JSON.parse(jsonReplica);
+        listReplicas.push(replicaObj);
+    }
+
+    res.status(listReplicas ? StatusCodes.OK : StatusCodes.NOTFOUND).send(listReplicas ? listReplicas : 'Not found.');
+})
 
 // Return single auction
 router.post('/:id', (req,res)=> {
@@ -46,7 +39,7 @@ router.post('/:id', (req,res)=> {
 
 
 // Search by parameter
-router.get('', (req,res, next)=> {
+router.get('/search', (req,res, next)=> {
     const filter = req.query.name;
     const filteredList = [];
     for (let replica of auctions) {
