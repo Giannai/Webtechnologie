61a9c7594ef02826331047e2c3edefae0d5e6c56
Giannai <500306@student.saxion.nl>
2021-09-27 15:39:48 +0200

    Auction api calls added filter with 3 attributes

--- src/routes/auctions.js
+++ src/routes/auctions.js
@@ -23,6 +23,7 @@ router.get('', (req,res)=> {
 
 // Return single auction
 router.post('/:id', (req,res)=> {
+
     let exists = false;
     for (const replica of auctions) {
         if (replica.id == req.params.id) {
@@ -32,14 +33,13 @@ router.post('/:id', (req,res)=> {
                 .send(replica);
         }
     }
-    if (!exists)
-        res
-            .send('Resource not found!');
+    res.status(!exists && StatusCodes.NOT_FOUND).send(!exists && 'Resource not found!');
 })
 
 
 // Search by parameter
-router.get('/search', (req,res, next)=> {
+router.get('/search', (req,res)=> {
+
     const filter = req.query.name;
     const filteredList = [];
     for (let replica of auctions) {
@@ -51,15 +51,49 @@ router.get('/search', (req,res, next)=> {
         }
     }
 
-    if (filteredList)
-        res
-            .status(StatusCodes.OK)
-            .send(filteredList);
-    else
-        res
-            .status(StatusCodes.NOT_FOUND)
-            .send('No results');
-
+    res.status(filteredList ? StatusCodes.OK : StatusCodes.NOTFOUND).send(filteredList ? filteredList : 'No results.');
 })
+
+
+// Filter the auction list with 3 different types of attributes
+router.get('/filter', (req,res)=> {
+
+    // Filter by attributes
+    // type
+    // system
+    // material
+
+    const attribs = [];
+
+    for (let key in req.query)
+        if (key != null)
+            attribs.push(key);
+
+    console.log('attributes: ' + attribs); // Print list of attributes used in query, it is possible to select 1 to 3
+
+    const values = [];
+    for (let i = 0; attribs.length > i; i += 1){
+        values.push(req.query[attribs[i]]);
+    }
+
+
+    console.log('values: ' + values); // Print all query parameters
+
+
+    // Filter all used parameters
+    const result = auctions.filter(function(e) {
+        return attribs.every(function(a) {
+            return values.includes(e[a.toLowerCase()]);
+        })
+    })
+    console.log(result);
+
+    res.status( result ? StatusCodes.OK : StatusCodes.BAD_REQUEST).send(result ? result : "Yup, this didn't work as expected, I’m probably not the best person to ask for that information.")
+})
+
+
+
+
+
 
 module.exports = router;
